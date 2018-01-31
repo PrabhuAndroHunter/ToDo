8a7b96f7a391930e9acd1901e18c9166a3ae4387
Unknown <prabhukumar.swamy@gmail.com>
2018-02-01 02:36:44 +0530

    one ScreenShot Added
    one ScreenShot Added

--- app/src/main/java/com/pub/todo/adapter/TaskViewAdapter.java
+++ app/src/main/java/com/pub/todo/adapter/TaskViewAdapter.java
@@ -14,6 +14,7 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.pub.todo.activity.HomeScreenActivity;
 import com.pub.todo.R;
@@ -77,7 +78,11 @@ public class TaskViewAdapter extends RecyclerView.Adapter <TaskViewAdapter.MyVie
             @Override
             public void onClick(View v) {
                 // when this event happen show dialog with prestored values
-                showDialogue(currentTask);
+                if (currentTask.isTaskCompleted()) {
+                    Toast.makeText(context, "Task Already Completed", Toast.LENGTH_SHORT).show();
+                } else {
+                    showDialogue(currentTask);
+                }
             }
         });
 
